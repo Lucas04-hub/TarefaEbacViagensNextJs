5d75ce0cd73e425c988d078e1472f13dc9ff63c2
Lucas <ribeirodasilvalucas918@gmail.com>
2026-07-08 16:52:10 -0700

    Correcao tarefa Next.js

---
+++ src/data/destinos.ts
@@ -0,0 +1,26 @@
+export const destinos = [
+  {
+    id: 1,
+    nome: 'Paris',
+    imagem: '/Paris.png',
+    descricao: 'A cidade do amor.',
+  },
+  {
+    id: 2,
+    nome: 'Australia',
+    imagem: '/Australia.png',
+    descricao: 'Aquiteturas unicas e piasagens incriveis.',
+  },
+  {
+    id: 3,
+    nome: 'Roma',
+    imagem: '/Roma.png',
+    descricao: 'Estruturas historicas intaticas.',
+  },
+  {
+    id: 4,
+    nome: 'Rio de Janeiro',
+    imagem: '/RioDeJaneiro.png',
+    descricao: 'Belezas naturais e praias relaxantes.',
+  },
+];
